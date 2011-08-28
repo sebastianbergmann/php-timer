a1ae8b93173343a9cfae4fb1fd83eeeb645000b4
Sebastian Bergmann <sb@sebastian-bergmann.de>
2011-08-28 12:37:27 +0200

    Include autoload script in list of files.

--- PHP/Timer/Autoload.php
+++ PHP/Timer/Autoload.php
@@ -56,7 +56,7 @@ function php_timer_autoload($class = NULL) {
     }
 
     if ($class === NULL) {
-        $result = array();
+        $result = array(__FILE__);
 
         foreach ($classes as $file) {
             $result[] = $path . $file;
