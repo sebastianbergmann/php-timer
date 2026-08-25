42cb35dde03865d12d0d510b713424d829804b6e
Sebastian Bergmann <sb@sebastian-bergmann.de>
2026-08-25 09:13:40 +0200

    Update tools

--- tools/.phpstan/vendor/phpstan/extension-installer/src/GeneratedConfig.php
+++ tools/.phpstan/vendor/phpstan/extension-installer/src/GeneratedConfig.php
@@ -35,8 +35,8 @@ final class GeneratedConfig
         0 => 'rules.neon',
       ),
     ),
-    'version' => '2.0.11',
-    'phpstanVersionConstraint' => '>=2.1.39.0-dev, <3.0.0.0-dev',
+    'version' => '2.0.12',
+    'phpstanVersionConstraint' => '>=2.1.52.0-dev, <3.0.0.0-dev',
   ),
   'tomasvotruba/type-coverage' => 
   array (
@@ -49,8 +49,8 @@ final class GeneratedConfig
         0 => 'config/extension.neon',
       ),
     ),
-    'version' => '2.1.0',
-    'phpstanVersionConstraint' => '>=2.0.0.0-dev, <3.0.0.0-dev',
+    'version' => '2.3.4',
+    'phpstanVersionConstraint' => '>=2.2.0.0-dev, <3.0.0.0-dev',
   ),
 );
 
@@ -58,7 +58,7 @@ final class GeneratedConfig
 );
 
 	/** @var string|null */
-	public const PHPSTAN_VERSION_CONSTRAINT = '>=2.1.39.0-dev, <3.0.0.0-dev';
+	public const PHPSTAN_VERSION_CONSTRAINT = '>=2.2.0.0-dev, <3.0.0.0-dev';
 
 	private function __construct()
 	{
